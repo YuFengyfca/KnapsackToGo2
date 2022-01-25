73303e96d1c7558113c6dbabfbadae29c4e20d98
YuFengyfca <yufengcayfca@gmail.com>
2022-01-26 01:31:25 +0800

    Add files via upload

--- src/cn/jji8/KnapsackToGo2/bungeeCord/xData/BungeeCordData.java
+++
@@ -1,9 +0,0 @@
-package cn.jji8.KnapsackToGo2.bungeeCord.xData;
-
-import cn.jji8.KnapsackToGo2.xdata.Data;
-/**
- * 用于与子服务器通信的类
- * */
-public class BungeeCordData extends Data {
-    //还没开始写，哈哈
-}
